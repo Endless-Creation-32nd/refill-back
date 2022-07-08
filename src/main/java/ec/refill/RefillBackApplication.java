385a6c0212b1537f9e16f02e7cd954240e1162c0
inhyeok <ss112d86@gmail.com>
2022-07-09 03:43:19 +0900

    chore: project init

---
+++ src/main/java/ec/refill/RefillBackApplication.java
@@ -0,0 +1,13 @@
+package ec.refill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefillBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefillBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/refill/RefillBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.refill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefillBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
