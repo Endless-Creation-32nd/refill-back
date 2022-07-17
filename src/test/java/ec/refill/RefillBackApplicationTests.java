46b6aa70fe352b9d43c6cfe8d633ee5b09a3468a
inhyeok <ss112d86@gmail.com>
2022-07-17 17:58:47 +0900

    feat: dev용 배포 docker compose  구축

--- src/test/java/ec/refill/RefillBackApplicationTests.java
+++ src/test/java/ec/refill/RefillBackApplicationTests.java
@@ -3,11 +3,11 @@ package ec.refill;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class RefillBackApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
