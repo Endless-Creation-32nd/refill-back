906ea0e6c43a3e018d29b9512bc35417c73bebe8
inhyeok <ss112d86@gmail.com>
2022-07-24 18:31:07 +0900

    feat: 관리자 멤버 조회 API

---
+++ src/main/java/ec/refill/domain/group/api/AdminApi.java
@@ -0,0 +1,31 @@
+package ec.refill.domain.group.api;
+
+import ec.refill.common.config.web.AuthMember;
+import ec.refill.common.config.web.LoginMember;
+import ec.refill.common.response.JsonResponse;
+import ec.refill.domain.group.application.AdminService;
+import ec.refill.domain.group.dto.GroupMemberDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/group/admin")
+public class AdminApi {
+
+  private final AdminService adminService;
+
+  @GetMapping("/{groupId}/members")
+  public ResponseEntity<?> getGroupMembers(@PathVariable("groupId") Long groupId,
+      @LoginMember AuthMember member) {
+
+    GroupMemberDto result = adminService.getGroupMembers(groupId, member.getId());
+
+    return JsonResponse.okWithData(HttpStatus.OK, "그룹 멤버 조회 성공", result);
+  }
+}
--- src/main/java/ec/refill/domain/group/application/AdminService.java
+++ src/main/java/ec/refill/domain/group/application/AdminService.java
@@ -1,12 +1,10 @@
 package ec.refill.domain.group.application;
 
+import ec.refill.common.exception.AuthorizationFailException;
 import ec.refill.common.exception.NotFoundResourceException;
 import ec.refill.domain.group.dao.GroupRepository;
-import ec.refill.domain.group.dao.ParticipationRepository;
 import ec.refill.domain.group.domain.Group;
-import ec.refill.domain.group.domain.Participation;
-import ec.refill.domain.group.domain.ParticipationStatus;
-import ec.refill.domain.group.exception.AlreadyParticipateException;
+import ec.refill.domain.group.dto.GroupMemberDto;
 import ec.refill.domain.member.dao.MemberRepository;
 import ec.refill.domain.member.domain.Member;
 import lombok.RequiredArgsConstructor;
@@ -15,25 +13,23 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
-public class ParticipateGroupService {
+public class AdminService {
 
-  private final ParticipationRepository participationRepository;
   private final MemberRepository memberRepository;
   private final GroupRepository groupRepository;
 
-  public void participate(Long groupId, Long memberId) {
+  @Transactional(readOnly = true)
+  public GroupMemberDto getGroupMembers(Long groupId, Long memberId){
     Member member = memberRepository.findById(memberId)
         .orElseThrow(() -> new NotFoundResourceException(memberId + "해당 유저를 잦을 수 없습니다."));
-
-    participationRepository.findByMemberAndParticipationStatus(member,
-        ParticipationStatus.PARTICIPATE).ifPresent((participation) -> {
-      throw new AlreadyParticipateException("이미 방에 참여중입니다.");
-    });
 
     Group group = groupRepository.findById(groupId)
         .orElseThrow(() -> new NotFoundResourceException("해당 그룹을 찾을 수 없습니다."));
 
-    participationRepository.save(new Participation(group, member, ParticipationStatus.PENDING));
+    if(!group.getAdminId().equals(memberId)){
+      throw new AuthorizationFailException("권한이 없습니다.");
+    }
+
+    return new GroupMemberDto(group);
   }
 }
